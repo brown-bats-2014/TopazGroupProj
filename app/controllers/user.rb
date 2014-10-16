a0436ca27c00a810a5d76c0efa4bf8272e503e50
thesock <jayme.mckiney@gmail.com>
2014-10-16 12:19:36 -0700

    users and session stuff

---
+++ app/controllers/user.rb
@@ -0,0 +1,14 @@
+post '/user/new' do
+  if params[:password] == params[:verify_password]
+    user = User.create(email: params[:email], password: params[:password])
+    if user.id.nil?
+      flash[:notice] = 'Email address has already been used'
+      redirect '/signup'
+    else
+      session[:user_id] = user.id
+      redirect '/'
+    end
+  else
+    flash[:notice] ="Your passwords do not match"
+  end
+end
