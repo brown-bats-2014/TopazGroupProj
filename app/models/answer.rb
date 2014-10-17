7ae3b7dbd7a2c01a0ec431482bef98f6f673d5c3
thesock <jayme.mckiney@gmail.com>
2014-10-17 12:05:00 -0700

    completed validation for composite primary key like behavior

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,12 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   belongs_to :choice
+  validate :only_choose_once
+
+  def only_choose_once
+    answer = Answer.find_by(user_id: self.user_id, question_id: self.question_id)
+    if !answer.nil? && self.id.nil?
+      self.errors[:base] <<  "You can only make a single choice for a given question on a survey"
+    end
+  end
 end
